a427da105350b25ea7749fcac310c9b9d425a98a
Adam Kalt <adam.kalt.1@gmail.com>
2018-09-03 03:42:27 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,5 +8,13 @@ validates :title, uniqueness: {
 validates :released, inclusion: { in: [true, false] }
 validates :artist_name, presence: true
 
+if validates :released, inclusion: { in: [true, false] }
+
+validates :release_year, presence: true, if: :released?
+  
+   def released?
+     released
+   end
+ end
 
 end
